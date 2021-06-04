4a84c36e0e05652df49edded976a48dd62daee2c
Alessio Proietti <alessio.proietti@protonmail.com>
2021-06-04 09:10:02 +0200

    added volume name

--- Run.sh
+++ Run.sh
@@ -2,4 +2,4 @@
 
 fhelib=$1
 
-docker run -d --name $fhelib-webide-container  -v test:/opt/CC/FHE-Workspace  -p 8443:8443 local/$fhelib-webide:latest
+docker run -d --name $fhelib-webide-container  -v $fhelib-workspace:/opt/CC/FHE-Workspace  -p 8443:8443 local/$fhelib-webide:latest
